1add581975ac8506715a8275ad0e68effe88627c
EdelMSena <edelmsena@gmail.com>
2026-09-10 13:02:16 -0400

    fixed hw

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
-    public static void main(String[] args){
-        System.out.println("What's up World!!!")
-    }
+  public static void main(String[] args){
+    System.out.println("What's up World!!!");
+  }
 }
